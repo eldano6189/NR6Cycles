cd14478a3d24838ccb942dd73bcfa1fb7caa39ac
eldano6189 <eldano6189@gmail.com>
2022-01-17 20:19:34 +0000

    added page transition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ScrollTop from "./Components/Scroll-Top/ScrollTop";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
@@ -8,15 +9,29 @@ import LandingPage from "./Pages/Landing-Page/LandingPage";
 import ProductsPage from "./Pages/Products-Page/ProductsPage";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <Header />
       <main>
         <ScrollTop>
-          <Routes>
-            <Route exact path="/" element={<LandingPage />} />
-            <Route path="/products" element={<ProductsPage authed={true} />} />
-          </Routes>
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={300}
+              classNames="page"
+              unmountOnExit
+            >
+              <Routes>
+                <Route exact path="/" element={<LandingPage />} />
+                <Route
+                  path="/products"
+                  element={<ProductsPage authed={true} />}
+                />
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
         </ScrollTop>
       </main>
       <Footer />
